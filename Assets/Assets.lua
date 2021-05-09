a4ffd7aa515ffd1d1db93f2a73142fd035f34c5b
Anielle Stellaire <anstellaire@protonmail.ch>
2021-05-09 03:44:18 +0300

    Add alpha parameter to colors

--- Assets/Assets.lua
+++ Assets/Assets.lua
@@ -14,17 +14,17 @@ EnnUF.Fonts = {
 
 EnnUF.Colors = {
     Health = {
-        Friend    = { r=0, g=1, b=0 },
-        Enemy     = { r=1, g=0, b=0 },
-        Neutral   = { r=1, g=1, b=0 },
-        Unflagged = { r=0, g=0.2, b=1 },
-        Unknown   = { r=1, g=1, b=1 }
+        Friend    = { r=0, g=1, b=0, a=1 },
+        Enemy     = { r=1, g=0, b=0, a=1 },
+        Neutral   = { r=1, g=1, b=0, a=1 },
+        Unflagged = { r=0, g=0.2, b=1, a=1 },
+        Unknown   = { r=1, g=1, b=1, a=1 }
     },
     Power = {
-        Mana    = { r=0.3, g=0.3, b=1 },
-        Rage    = { r=1, g=0.3, b=0.3 },
-        Focus   = { r=1, g=0.6, b=0.3 },
-        Energy  = { r=1, g=1, b=0.3 },
-        Unknown = { r=1, g=1, b=1 }
+        Mana    = { r=0.3, g=0.3, b=1, a=1 },
+        Rage    = { r=1, g=0.3, b=0.3, a=1 },
+        Focus   = { r=1, g=0.6, b=0.3, a=1 },
+        Energy  = { r=1, g=1, b=0.3, a=1 },
+        Unknown = { r=1, g=1, b=1, a=1 }
     }
 }
