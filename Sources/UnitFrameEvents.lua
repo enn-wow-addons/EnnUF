891b116737bf218dbf3f1e7542675da14f060e4c
Anielle Stellaire <anstellaire@protonmail.ch>
2022-05-11 01:24:31 +0300

    Refactor unit frame events

--- Sources/UnitFrameEvents.lua
+++ Sources/UnitFrameEvents.lua
@@ -1,17 +1,18 @@
+------------------------
+-- Unit Frame Helpers --
+------------------------
 
-------------------------------
--- Unit Frame Event Handles --
-------------------------------
-
-EnnUF_UnitFrame_EventHandle = {}
-
-EnnUF_UnitFrame_EventHandle["ADDON_LOADED"] = function(uf)
+local function UpdateUnitFrameFull(uf)
     if EnnUF_UnitExists(uf.unitid) then
-        uf:UpdateHealth(EnnUF_GetHealthPercentage(uf.unitid), EnnUF_UnitHealth(uf.unitid));
-        uf:UpdatePower(EnnUF_GetPowerPercentage(uf.unitid), EnnUF_UnitPower(uf.unitid));
+        uf:UpdateHealth(
+            EnnUF_GetHealthPercentage(uf.unitid), EnnUF_UnitHealth(uf.unitid));
+        uf:UpdateHealthColor(
+            EnnUF_GetHealthColor(uf.unitid));
 
-        uf:UpdateHealthColor(EnnUF_GetHealthColor(uf.unitid));
-        uf:UpdatePowerColor(EnnUF_GetPowerColor(uf.unitid));
+        uf:UpdatePower(
+            EnnUF_GetPowerPercentage(uf.unitid), EnnUF_UnitPower(uf.unitid));
+        uf:UpdatePowerColor(
+            EnnUF_GetPowerColor(uf.unitid));
 
         uf:Show();
     else
@@ -19,39 +20,17 @@ EnnUF_UnitFrame_EventHandle["ADDON_LOADED"] = function(uf)
     end
 end
 
-EnnUF_UnitFrame_EventHandle["PLAYER_ENTERING_WORLD"] = function(uf)
-    if EnnUF_UnitExists(uf.unitid) then
-        uf:UpdateHealth(EnnUF_GetHealthPercentage(uf.unitid), EnnUF_UnitHealth(uf.unitid));
-        uf:UpdatePower(EnnUF_GetPowerPercentage(uf.unitid), EnnUF_UnitPower(uf.unitid));
-
-        uf:UpdateHealthColor(EnnUF_GetHealthColor(uf.unitid));
-        uf:UpdatePowerColor(EnnUF_GetPowerColor(uf.unitid));
-
-        uf:Show();
-    else
-        uf:Hide();
-    end
-end
-
-EnnUF_UnitFrame_EventHandle["UNIT_HEALTH"] = function(uf)
+local function UpdateUnitFrameHealth(uf)
     uf:UpdateHealth(EnnUF_GetHealthPercentage(uf.unitid), EnnUF_UnitHealth(uf.unitid));
 end
 
-EnnUF_UnitFrame_EventHandle["UNIT_HEALTH_FREQUENT"] = function(uf)
-    uf:UpdateHealth(EnnUF_GetHealthPercentage(uf.unitid), EnnUF_UnitHealth(uf.unitid));
-end
-
-EnnUF_UnitFrame_EventHandle["UNIT_MANA"] = function(uf)
+local function UpdateUnitFramePower(uf)
     uf:UpdatePower(EnnUF_GetPowerPercentage(uf.unitid), EnnUF_UnitPower(uf.unitid));
 end
 
-EnnUF_UnitFrame_EventHandle["UNIT_ENERGY"] = function(uf)
-    uf:UpdatePower(EnnUF_GetPowerPercentage(uf.unitid), EnnUF_UnitPower(uf.unitid));
-end
-
-EnnUF_UnitFrame_EventHandle["UNIT_AURA"] = function(uf)
-    uf:UpdateHealth(EnnUF_GetHealthPercentage(uf.unitid), EnnUF_UnitHealth(uf.unitid));
-    uf:UpdatePower(EnnUF_GetPowerPercentage(uf.unitid), EnnUF_UnitPower(uf.unitid));
+local function UpdateUnitFrameSpecialAura(uf)
+    UpdateUnitFrameHealth(uf)
+    UpdateUnitFramePower(uf)
 
     local _, unitClass = EnnUF_UnitClass(uf.unitid);
     if unitClass == "DRUID" then
@@ -59,20 +38,27 @@ EnnUF_UnitFrame_EventHandle["UNIT_AURA"] = function(uf)
     end
 end
 
-EnnUF_UnitFrame_EventHandle["UNIT_FLAGS"] = function(uf)
+local function UpdateUnitFrameSpecialFlags(uf)
     uf:UpdateHealthColor(EnnUF_GetHealthColor(uf.unitid));
 end
 
-EnnUF_UnitFrame_EventHandle["PLAYER_TARGET_CHANGED"] = function(uf)
-    if EnnUF_UnitExists(uf.unitid) then
-        uf:UpdateHealth(EnnUF_GetHealthPercentage(uf.unitid), EnnUF_UnitHealth(uf.unitid));
-        uf:UpdatePower(EnnUF_GetPowerPercentage(uf.unitid), EnnUF_UnitPower(uf.unitid));
+------------------------------
+-- Unit Frame Event Handles --
+------------------------------
 
-        uf:UpdateHealthColor(EnnUF_GetHealthColor(uf.unitid));
-        uf:UpdatePowerColor(EnnUF_GetPowerColor(uf.unitid));
+EnnUF_UnitFrame_EventHandle = {}
 
-        uf:Show();
-    else
-        uf:Hide();
-    end
-end
+EnnUF_UnitFrame_EventHandle["ADDON_LOADED"] = UpdateUnitFrameFull
+EnnUF_UnitFrame_EventHandle["PLAYER_ENTERING_WORLD"] = UpdateUnitFrameFull
+EnnUF_UnitFrame_EventHandle["PLAYER_TARGET_CHANGED"] = UpdateUnitFrameFull
+EnnUF_UnitFrame_EventHandle["PLAYER_FOCUS_CHANGED"] = UpdateUnitFrameFull
+EnnUF_UnitFrame_EventHandle["UNIT_TARGET"] = UpdateUnitFrameFull
+
+EnnUF_UnitFrame_EventHandle["UNIT_HEALTH"] = UpdateUnitFrameHealth
+EnnUF_UnitFrame_EventHandle["UNIT_HEALTH_FREQUENT"] = UpdateUnitFrameHealth
+
+EnnUF_UnitFrame_EventHandle["UNIT_MANA"] = UpdateUnitFramePower
+EnnUF_UnitFrame_EventHandle["UNIT_ENERGY"] = UpdateUnitFramePower
+
+EnnUF_UnitFrame_EventHandle["UNIT_AURA"] = UpdateUnitFrameSpecialAura
+EnnUF_UnitFrame_EventHandle["UNIT_FLAGS"] = UpdateUnitFrameSpecialFlags
